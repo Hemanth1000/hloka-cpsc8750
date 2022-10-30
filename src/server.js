566f485c05c9b3e3ee79c0b1c2f22ecfbe00cabf
Hemanth Reddy Loka <lokahemanthreddy@gmail.com@gmail.com>
2022-10-30 09:27:42 -0500

    APIlayer assignment

--- src/server.js
+++ src/server.js
@@ -1,63 +1,105 @@
+const fetch = require('node-fetch');
 // use the express library
 const express = require('express');
+
 // create a new server application
 const app = express();
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+
 // Define the port we will listen on
 // (it will attempt to read an environment global
 // first, that is for when this is used on the real
 // world wide web).
 const port = process.env.PORT || 3000;
+
 let nextVisitorId = 1;
+const cookieParser = require('cookie-parser');
+// ... snipped out code ...
 
-// Function to calculate for how long you have been here
-function dateTimeFunction(lastAccessedTime) {
-    var timeOnSite = Date.now() - lastAccessedTime;
-    var totalTime = timeOnSite / 1000;
-    var timeInSeconds = Math.floor(totalTime);
-    return timeInSeconds;
-}
+app.use(cookieParser());
 
-// The main page of our website
+app.get("/trivia", async (req, res) => {
+    // fetch the data
+    const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+    // fail if bad response
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
+
+    // interpret the body as json
+    const content = await response.json();
+
+    // fail if db failed
+    if (content.response_code !== 0) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+        return;
+    }
+
+    const responseData = content.results[0]
+    var correct_answers = []
+    correct_answers.push(responseData.correct_answer);
+    correct_and_incorrect_answers = correct_answers.concat(responseData.incorrect_answers);
+
+    let answers = correct_and_incorrect_answers
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+
+    const answerLinks = answers.map(answer => {
+        return `<a href="javascript:alert('${answer === responseData.correct_answer ? 'Correct!' : 'Incorrect, Please Try Again!'
+            }')">${answer}</a>`
+    })
+    console.log(correct_and_incorrect_answers);
+
+    // respond to the browser
+    // TODO: make proper html
+    res.render('trivia', {
+        question: responseData.question,
+        category: responseData.category,
+        difficulty: responseData.difficulty,
+        answers: answerLinks
+    })
+
+
+});
+
 app.get('/', (req, res) => {
 
     if (isNaN(req.cookies['visitorId'])) {
         nextVisitorId++;
     }
+    res.cookie('visitorId', nextVisitorId);
 
-    res.cookie('visitorId', nextVisitorId)
 
-    // To check for first time
-    var output = "";
-    if (isNaN(dateTimeFunction(req.cookies['visited']))) {
-        output = "Welcome to this website";
-    } else {
-        var time = dateTimeFunction(req.cookies['visited']);
-        if (time < 60) {
-            output = "It has been " + time + " seconds since your last visit";
-        } else {
-            output = "It has been " + Math.floor((time % 3600) / 60) + " minutes since your last visit";
-        }
+    var last_visit = "";
+    var last_tried = req.cookies['visited'];
+    var lstTime = Math.floor((Date.now() - last_tried) / 1000);
+    if (isNaN(lstTime)) {
+        last_visit = "You have never visited";
     }
-
-    res.cookie('visited', Date.now().toString());
+    else {
+        last_visit = "It has been " + lstTime + " seconds since your last visit";
+    }
+    res.cookie('visited', Date.now());
     res.render('welcome', {
-        name: req.query.name || "World",
-        last_accessed: new Date().toLocaleString() || "10/09/2022, 00:00:00 PM",
-        visitedNo: `${output}`,
-        visitorId: req.cookies['visitorId'] || nextVisitorId
+        name: req.query.name || "World!!",
+        last_tried: new Date().toLocaleString() || "10/9/2022 , 00:00:00 AM",
+        how_long: `${last_visit}`,
+        id: req.cookies['visitorId'] || nextVisitorId
     });
-
-    console.log(req.headers.cookie);
 });
-
-app.use(express.static('public'));
 
 // Start listening for network connections
 app.listen(port);
 
 // Printout for readability
 console.log("Server Started!");
+
+// To tell about public folder
+app.use(express.static('public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
